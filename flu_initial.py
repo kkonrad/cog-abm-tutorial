67bcf239817728fda995b7bca00788325c59f713
Konrad Kurdej <kkonrad@i_wont_tell.com>
2012-11-12 14:36:49 +0100

    Easier flu

--- flu_initial.py
+++ flu_initial.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('/home/konrad/ICM/cog-abm-tutorial/COG-ABM/src')
+# TODO adapt to your instalations^^^^
 
 import random
 
@@ -16,6 +17,47 @@ CURE_PROB = 0.95
 ITERS = 500000
 DUMP_FREQ = ITERS / 100
 
+
+class FluAgentState(object):
+
+    # TODO
+
+
+class FluInteraction(Interaction):
+
+    def num_agents(self):
+        return 2
+
+    def interact(self, agent1, agent2):
+        ags1, ags2 = agent1.state, agent2.state
+        # TODO
+
+
+def prepare_agents(num_agents, num_doctors, num_ill):
+    agents = [Agent(state=FluAgentState())
+        for _ in xrange(num_agents)]
+    #TODO and adapt ^^^^
+
+
+def flu_experiment(num_agents, num_doctors, num_ill, iters):
+    agents = prepare_agents(num_agents, num_doctors, num_ill)
+    topology = generate_simple_network(agents)
+    s = Simulation(topology, FluInteraction(), agents)
+    return s.run(iters, DUMP_FREQ)
+
+
+def analyze(results):
+    def tmp(agents):
+        return avg([ for agent in agents])
+        # TODO ^^^^^^ 1 - infected, 0 - not infected
+    return [(it, tmp(agents)) for it, agents in results]
+
+
+def present_results(analysis):
+    import pprint
+    pprint.pprint(analysis)
+    from presenter.charts import wykres
+    wykres(analysis, "iteration", "ill percentage")
 
 
 def main(args):
